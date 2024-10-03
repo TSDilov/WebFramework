f30d530df9f6087679752b291906fbc48eb526db
TSDilov <cv.dilov@abv.bg>
2024-10-03 14:56:49 +0300

    Adding simple Router

--- WebFramework/WebFramework/Core/HttpServer.cs
+++ WebFramework/WebFramework/Core/HttpServer.cs
@@ -3,14 +3,19 @@
     using System.Net.Sockets;
     using System.Net;
     using System.Text;
+    using WebFramework.Routing;
 
     public class HttpServer
     {
         private readonly TcpListener _listener;
+        private readonly Router _router = new Router();
 
         public HttpServer(int port)
         {
             _listener = new TcpListener(IPAddress.Any, port);
+
+            _router.RegisterRoute("/", () => "HTTP/1.1 200 OK\r\n\r\nWelcome to the Home Page!");
+            _router.RegisterRoute("/about", () => "HTTP/1.1 200 OK\r\n\r\nAbout this Web Framework");
         }
 
         public async Task StartAsync()
@@ -32,14 +37,10 @@
             var length = await stream.ReadAsync(buffer, 0, buffer.Length);
             var request = Encoding.UTF8.GetString(buffer, 0, length);
 
-            Console.WriteLine(request);
+            var requestLine = request.Split("\r\n")[0];
+            var path = requestLine.Split(" ")[1];
 
-            var response = "HTTP/1.1 200 OK\r\n" +
-                           "Content-Type: text/plain\r\n" +
-                           "Content-Length: 11\r\n" +
-                           "\r\n" +
-                           "Hello World";
-
+            var response = _router.ResolveRoute(path);
             var responseBytes = Encoding.UTF8.GetBytes(response);
             await stream.WriteAsync(responseBytes, 0, responseBytes.Length);
 
---
+++ WebFramework/WebFramework/Routing/Router.cs
@@ -0,0 +1,22 @@
+﻿namespace WebFramework.Routing
+{
+    public class Router
+    {
+        private readonly Dictionary<string, Func<string>> _routes = new Dictionary<string, Func<string>>();
+
+        public void RegisterRoute(string path, Func<string> action)
+        {
+            _routes[path] = action;
+        }
+
+        public string ResolveRoute(string path)
+        {
+            if (_routes.ContainsKey(path))
+            {
+                return _routes[path]();
+            }
+
+            return "HTTP/1.1 404 Not Found\r\n\r\nPage not found";
+        }
+    }
+}
